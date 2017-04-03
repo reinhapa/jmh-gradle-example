b0843a6e8b4a6c136978caab9e71e61403261417
Patrick Reinhart <patrick@reini.net>
2017-04-03 22:28:45 +0200

    Adds simple UID benchmark

---
+++ src/jmh/java/tech/bison/benchmark/IdGeneration.java
@@ -0,0 +1,78 @@
+/**
+ * File Name: AttributeNameToPosition.java
+ * 
+ * Copyright (c) 2016 BISON Schweiz AG, All Rights Reserved.
+ */
+
+package tech.bison.benchmark;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+@State(Scope.Thread)
+public class IdGeneration {
+
+	@Benchmark
+	@BenchmarkMode(Mode.Throughput)
+	@OutputTimeUnit(TimeUnit.SECONDS)
+	public void measureThroughput_UUID(Blackhole bh) {
+		bh.consume(UUID.randomUUID().toString());
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	public void measureAvgTime_UUID(Blackhole bh) {
+		bh.consume(UUID.randomUUID().toString());
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.Throughput)
+	@OutputTimeUnit(TimeUnit.SECONDS)
+	public void measureThroughput_BisonUUID(Blackhole bh) {
+		bh.consume(BisonUID.nextId());
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	public void measureAvgTime_BisonUUID_extended(Blackhole bh) {
+		bh.consume(BisonUID.nextExtendedId());
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.Throughput)
+	@OutputTimeUnit(TimeUnit.SECONDS)
+	public void measureThroughput_BisonUUID_extended(Blackhole bh) {
+		bh.consume(BisonUID.nextExtendedId());
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.MICROSECONDS)
+	public void measureAvgTime_BisonUUID(Blackhole bh) {
+		bh.consume(BisonUID.nextId());
+	}
+
+	static class BisonUID {
+		private static final AtomicLong COUNTER = new AtomicLong();
+
+		public static String nextId() {
+			return Long.toString(COUNTER.incrementAndGet(), Character.MAX_RADIX);
+		}
+
+		public static String nextExtendedId() {
+			return new StringBuilder().append(Long.toString(System.nanoTime(), Character.MAX_RADIX)).append('-')
+					.append(Long.toString(COUNTER.incrementAndGet(), Character.MAX_RADIX)).toString();
+		}
+	}
+}
